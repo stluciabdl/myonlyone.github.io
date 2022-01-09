e34ec82455a7053baa42dab701e928b84c729dac
Overthinking-Velociraptor <92566523+Overthinking-Velociraptor@users.noreply.github.com>
2022-01-09 20:59:47 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+const clock = () => {
+  var date    = new Date();
+  var hours   = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  var midday;
+
+  hours     = updateTime(hours);
+  minutes   = updateTime(minutes);
+  seconds   = updateTime(seconds);
+  var name  = "❤️Bella❤️";
+
+  // if else condition
+
+  midday = (hours >= 12) ? "PM" : "AM";
+
+  document.getElementById("clock").innerHTML =
+    ""+hours+"" + ":" + ""+minutes+"" + ":" + ""+seconds+"" + ""+midday+"";
+
+  var time = setTimeout(function () {
+    clock();
+  }, 1000);
+
+  //   Good Morning and Good Night Conditions
+
+  if (hours < 12) {
+    var greeting = "Good Morning " + name + "Have a nice day";
+  }
+
+  if (hours >= 12 && hours <= 18) {
+    var greeting = "Good Afternoon " + name + "Jangan lupa minum biar ngga dehidrasi";
+  }
+
+  if (hours >= 18 && hours <= 24) {
+    var greeting = "Good Evening " + name;
+  }
+
+  document.getElementById("greetings").innerHTML = greeting;
+}
+
+const updateTime = (k) => {
+  if (k < 10) {
+    return "0" + k;
+  } else {
+    return k;
+  }
+}
+
+// call clock function 
+clock();
